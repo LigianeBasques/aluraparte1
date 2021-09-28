2471dafe39616e3a3ea3987c418f2c963fe622ca
Ligiane <ligianealzie25@gmail.com>
2021-09-28 10:59:47 -0300

    for

--- comandofor.py
+++ comandofor.py
@@ -1,8 +1,4 @@
 
-i = int(input('Inicio: '))
-f = int(input('Fim: '))
-p = int(input('Passo: '))
-
-for c in range(i,f+1,p):
-    print(c)
+for c in range(0,10):
+    n = int(input('Digite um valor: '))
 print('FIM')
